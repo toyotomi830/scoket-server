b971f7757bb4c9fa25bce957f496d7d593fbd4f2
Runtong <toyotomi830@gmail.com>
2022-05-09 21:12:37 +0800

    basic text channel

--- socket.js
+++ socket.js
@@ -2,7 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const res = require('express/lib/response');
+const fs = require("fs");
 
+let channelStr=fs.readFileSync(__dirname+'/server_config/channel.json');
+let channel = JSON.parse(channelStr).channelList;
 http.listen(80);
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
@@ -14,8 +17,11 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function(){
     console.log('disconnected');
   });
-  socket.on('message',function(msg){
-    socket.broadcast.emit('message',msg)
-    console.log('recived '+msg);
+  channel.forEach(element => {
+    socket.on(element.name,function(msg){
+      socket.broadcast.emit(element.name,msg)
+      console.log(element.name+'channel recived '+msg);
+    });
   });
+
 });  
